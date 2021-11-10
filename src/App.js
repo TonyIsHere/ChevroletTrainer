4ec47540b4206adc49d52f2a0163b7a3cb77f8b3
TonyIsHere <anthony.chevrolet.1217@gmail.com>
2021-11-10 16:18:58 +0200

    Implement React router and Tabs from mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import CustomersList from "./components/customerlist";
+
+
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+function Home() {
+  return (<div>Hello
+
+
+  </div>)
+}
+
+function About() {
+  return (<div>About</div>)
+}
+
 function App() {
+
+  const [value, setValue] = useState(0);
+  const handleChangeTab = (event, value) => {
+    setValue(value);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AppBar position="static">
+          <Tabs value={value}
+            onChange={handleChangeTab}
+            aria-label="nav tabs example"
+            indicatorColor="secondary"
+            textColor="inherit">
+            <Tab label="Page One" to="/" component={Link} />
+            <Tab label="Page Two" to="/JRM" component={Link} />
+            <Tab label="Page Three" to="/about" component={Link} />
+          </Tabs>
+        </AppBar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<CustomersList />}></Route>
+          <Route path="/contact" element={<h1>Contact address</h1>} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/customerlist.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+function CustomersList()
+{
+    return(<div>Yo</div>);
+}
+
+export default CustomersList;
